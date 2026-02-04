bcc09e65f7edfefa92bd367126893679f34748ea
ichi <realkouu@gmail.com>
2026-02-04 12:31:16 +0900

    stopwatch

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 福田光一郎 on 2026/02/04.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
